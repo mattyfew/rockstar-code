e90349cd166789f4b4cffd2ecbdf31e124a5bbb3
Matt Fewer <mattfewer@hotmail.com>
2018-09-30 16:52:08 +0200

    everything spaced out ok on home page

--- wp-content/themes/rockstar/page-about-me.php
+++ wp-content/themes/rockstar/page-about-me.php
@@ -1,10 +1,43 @@
 <?php get_header(); ?>
 
+
+
+
+
+<div id="main-container">
+    <h1><?php wp_title(''); ?></h1>
+
+    <p><?php the_content(); ?></p>
+
     <?php
         if( have_posts() ):
             while( have_posts() ): the_post(); ?>
                 <p><?php the_content(); ?></p>
             <?php endwhile;
         endif; ?>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 <?php get_footer(); ?>
